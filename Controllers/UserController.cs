1b889029e47f7171526ab82d6cd5ccdc3b725c67
thinhnd <thinhnd@THINHND>
2024-01-22 17:19:47 +0700

    xoa css update trang tong quat

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Xproduct.Models;
 
 namespace Xproduct.Controllers
 {
     public class UserController : Controller
     {
+        DBContext _db = new DBContext();
         // GET: User
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var listUser = new DBContext().Users.ToList();
+            return View(listUser);
         }
+
+
+        //[HttpPost]
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
     }
+     
+   
 }
